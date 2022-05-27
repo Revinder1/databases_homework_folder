bd932089391ed181ed753457a0d5755e1d411fea
Yaroslav <67860564+Revinder1@users.noreply.github.com>
2022-05-27 23:30:04 +0500

    Update script_for_db.py

--- homework1_2/script_for_db.py
+++ homework1_2/script_for_db.py
@@ -1,1 +1,19 @@
+create table if not exists artists (
+	id serial primary key,
+	name varchar(40) unique not null
+);
+
+create table if not exists albums (
+	id serial primary key,
+	name varchar(40) unique not null,
+	year date,
+	artist_id integer references artists(id)
+);
+
+create table if not exists song (
+	id serial primary key,
+	name varchar(40) unique not null,
+	length time not null,
+	album_id integer references albums(id)
+); 
 
